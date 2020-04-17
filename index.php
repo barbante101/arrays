9208cbf684b78e73223be30ad83013eda29486f7
Roldan Barbante <rbarbante@mail.greenriverdev.com>
2020-04-17 16:45:54 -0400

    complete step one

---
+++ index.php
@@ -0,0 +1,36 @@
+/*
+Roldan Barbante
+4/17/20
+https://github.com/barbante101/arrays.git
+Pair program 2
+
+turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+*/
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program</title>
+</head>
+<body>
+<?php
+
+    echo "PHP Array Practice";
+
+
+?>
+
+</body>
+</html>
+
+
+
+
+
